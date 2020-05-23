40a3c78954d4ce3d9e2f12591ebdedd2a2379671
varunr10 <63897023+varunr10@users.noreply.github.com>
2020-05-24 01:38:03 +0530

    Added the really stupid leetcode problem
    that has a really cool solution

---
+++ RomanToInteger.java
@@ -0,0 +1,48 @@
+
+/*
+For Complete Problem statement, check lookup the leetcode problem No.13
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+For example, two is written as II in Roman numeral, just two one's added together.
+Twelve is written as, XII, which is simply X + II.
+Twenty seven is written as XXVII, which is XX + V + II.
+
+*/
+
+class Solution {
+    public int romanToInt(String s) {
+     int sum=0;
+    if(s.indexOf("IV")!=-1){sum-=2;}
+    if(s.indexOf("IX")!=-1){sum-=2;}
+    if(s.indexOf("XL")!=-1){sum-=20;}
+    if(s.indexOf("XC")!=-1){sum-=20;}
+    if(s.indexOf("CD")!=-1){sum-=200;}
+    if(s.indexOf("CM")!=-1){sum-=200;}
+    
+    char c[]=s.toCharArray();
+    int count=0;
+    
+   for(;count<=s.length()-1;count++){
+       if(c[count]=='M') sum+=1000;
+       if(c[count]=='D') sum+=500;
+       if(c[count]=='C') sum+=100;
+       if(c[count]=='L') sum+=50;
+       if(c[count]=='X') sum+=10;
+       if(c[count]=='V') sum+=5;
+       if(c[count]=='I') sum+=1;
+       
+   }
+   
+   return sum;
+    
+}
+}
