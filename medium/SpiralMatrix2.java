ee7d0603dc23de38432c4764a930df384fc2caa2
varunr10 <63897023+varunr10@users.noreply.github.com>
2020-08-05 21:38:11 +0530

    Added solution to SpiralMatrix2.java
    Let go

--- medium/SpiralMatrix2.java
+++ medium/SpiralMatrix2.java
@@ -14,8 +14,39 @@ Output:
 
 public class SpiralMatrix2 {
     public int[][] generateMatrix(int n) {
-    
-    //Coming jooon tomorrow
-    
+        
+        int[][] res = new int[n][n];
+        
+        int rowBegin = 0;
+        int rowEnd = n - 1;
+        int colBegin = 0;
+        int colEnd = n - 1;
+        int counter = 1;
+        while(rowBegin <= rowEnd && colBegin <= colEnd){
+            for(int i = colBegin; i <= colEnd; i++){
+                res[rowBegin][i]=counter++;
+            }
+            rowBegin++;
+            
+            for(int i= rowBegin; i <= rowEnd; i++){
+                res[i][colEnd]=counter++;
+            }
+            colEnd--;
+            
+            if(rowBegin <= rowEnd){
+            for(int i = colEnd; i >= colBegin; i--){
+                res[rowEnd][i]=counter++;                
+            }
+            }
+            rowEnd--;
+            
+            if(colBegin <= colEnd){
+            for(int i= rowEnd; i >= rowBegin; i--){
+                res[i][colBegin]=counter++;
+            }
+            }
+            colBegin++;
+        }
+        return res;
     }
 }
