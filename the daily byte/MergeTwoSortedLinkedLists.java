b0e10e36112ec69ee448de4e3bec55777df69f3b
varunr10 <63897023+varunr10@users.noreply.github.com>
2020-09-28 00:30:37 +0530

    Added question asked by Google

---
+++ the daily byte/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,40 @@
+package com.dailybyte;
+
+/*		Leetcode problem 21 	*/ 
+public class MergeTwoSortedLinkedList {
+
+	public static ListNode mergeTwoSortedLinkedLists(ListNode l1, ListNode l2) {
+		
+		ListNode dummy = new ListNode(-1);
+		ListNode head = dummy;
+		
+		while(l1 != null && l2 != null) {
+			if(l2.val < l1.val) {
+				dummy.next = l2;
+				l2 = l2.next;
+			}
+			else {
+				dummy.next = l1;
+				l1 = l1.next;
+			}
+			dummy = dummy.next;
+		}
+		
+		if(l1 != null) {
+			dummy.next = l1;
+		}
+		if(l2 != null) {
+			dummy.next = l2;
+		}
+		
+		return head.next;
+	}
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
