c7b9ea093aefa1669f5dfa430836c4a23e6bbc63
varunr10 <63897023+varunr10@users.noreply.github.com>
2020-09-29 00:54:38 +0530

    Added Day_16_RemoveNthToLastNode.java

--- the daily byte/Day_16_RemoveNthToLastNode.java
+++ the daily byte/Day_16_RemoveNthToLastNode.java
@@ -8,3 +8,29 @@
   1->2->3->null, n = 3, return 2->3->null
 
   */
+package com.dailybyte;
+
+public class RemoveNthToLastNode {
+
+	public static ListNode removeNthToLastNode(ListNode head, int n) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		
+		ListNode fast = head;
+		ListNode slow = head;
+		
+		for(int i = 1; i <= n+1; i++) {
+			fast = fast.next;
+		}
+		
+		while(fast != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+		slow.next = slow.next.next;
+		
+		return dummy.next;
+		
+	}
+}
+
