ac5195cb626293f375d294465036fce431b793e0
varunr10 <63897023+varunr10@users.noreply.github.com>
2021-02-01 09:40:52 +0530

    Added ParkingSystem.java

---
+++ easy/ParkingSystem.java
@@ -0,0 +1,59 @@
+/*
+Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small, with a fixed number of slots for each size.
+
+Implement the ParkingSystem class:
+
+ParkingSystem(int big, int medium, int small) Initializes object of the ParkingSystem class. The number of slots for each parking space are given as part of the constructor.
+bool addCar(int carType) Checks whether there is a parking space of carType for the car that wants to get into the parking lot. carType can be of three kinds: big, medium, or small
+which are represented by 1, 2, and 3 respectively. 
+
+A car can only park in a parking space of its carType. If there is no space available, return false, else park the car in that size space and return true.
+
+Input
+["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+[[1, 1, 0], [1], [2], [3], [1]]
+Output
+[null, true, true, false, false]
+
+Explanation
+ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+parkingSystem.addCar(1); // return true because there is 1 available slot for a big car
+parkingSystem.addCar(2); // return true because there is 1 available slot for a medium car
+parkingSystem.addCar(3); // return false because there is no available slot for a small car
+parkingSystem.addCar(1); // return false because there is no available slot for a big car. It is already occupied.
+*/
+
+public class ParkingSystem {
+
+    public static int bigSpots = 0;
+    public static int mediumSpots = 0;
+    public static int smallSpots = 0;
+    
+    public ParkingSystem(int big, int medium, int small) {
+        bigSpots = big;
+        mediumSpots = medium;
+        smallSpots = small;
+    }
+    
+    public boolean addCar(int carType) {
+        if(carType == 1 && bigSpots > 0){
+            bigSpots -= 1;
+            return true;
+        }
+        if(carType == 2 && mediumSpots > 0){
+            mediumSpots -= 1;
+            return true;
+        }
+        if(carType == 3 && smallSpots > 0){
+            smallSpots -= 1;
+            return true;
+        }
+        return false;    
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
