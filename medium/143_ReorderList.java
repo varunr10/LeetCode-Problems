cc69e2767d47447379b5ab1d406bbbd0a6518ddd
varunr10 <63897023+varunr10@users.noreply.github.com>
2022-05-15 22:05:14 +0530

    trick: Split ->2 list, reverse 2nd list, loop both

--- medium/143_ReorderList.java
+++ medium/143_ReorderList.java
@@ -1,5 +1,19 @@
-package linkedlists;
+/*
+[1,2,3,4,5]
+[1,5,2,4,3]
+Step1: Split the linked list into two halves by finding the middle using fast/slow pointers.
+	First half = 1,2,3
+	Second half = 4,5
+Step2: Reverse the second half
+	Second half = 5,4
 
+Step3: Traverse both lists together and merge both the lists
+	First Half = 1,2,3
+	Second half = 5,4
+
+Output = [1,5,2,4,3]
+
+*/
 public class ReorderList {
 	public static void reorderList(ListNode head) {
 		if(head == null || head.next == null)
@@ -17,7 +31,7 @@ public class ReorderList {
 
 		ListNode list2 = reverseLinkedList(slow);
 
-		mergeTwoSortedLinkedLists(head, list2);
+		mergeTwoLinkedLists(head, list2);
 	}
 
 	public static ListNode reverseLinkedList(ListNode head) {
@@ -34,7 +48,7 @@ public class ReorderList {
 		return prev;
 	} 
 
-	public static void mergeTwoSortedLinkedLists(ListNode list1, ListNode list2) {
+	public static void mergeTwoLinkedLists(ListNode list1, ListNode list2) {
 		while(list1 != null) {
 			ListNode one_next = list1.next;
 			ListNode two_next = list2.next;
