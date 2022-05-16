7fb2ff7828af561c3d05c83ea2dada5ed98980c4
varunr10 <63897023+varunr10@users.noreply.github.com>
2022-05-16 21:44:28 +0530

    approach: Maintain Odd list & Even list, link them

--- medium/328_OddEvenLinkedList.java
+++ medium/328_OddEvenLinkedList.java
@@ -5,11 +5,10 @@ Please note here we are talking about the node number and not the value in the n
 You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
 
 Example 1:
-
 Input: 1->2->3->4->5->NULL
 Output: 1->3->5->2->4->NULL
-Example 2:
 
+Example 2:
 Input: 2->1->3->5->6->4->7->NULL
 Output: 2->3->6->7->1->5->4->NULL
 
